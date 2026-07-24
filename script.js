c745ed50cf1a1ef46251c37fa945ef3df0274cd5
hhasansunnahshop-dev <hhasansunnahshop@gmail.com>
2026-07-24 19:23:36 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+document.getElementById("orderForm").addEventListener("submit", function(e) {
+    e.preventDefault();
 
+    document.getElementById("msg").innerHTML =
+        "ধন্যবাদ! আপনার অর্ডার গ্রহণ করা হয়েছে।";
+
+    this.reset();
+});
