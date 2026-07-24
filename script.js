85d00f8df44d8bd044befad24c0b547ccb36678b
hhasansunnahshop-dev <hhasansunnahshop@gmail.com>
2026-07-24 19:48:03 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-document.getElementById("orderForm").addEventListener("submit", function(e) {
+document.getElementById("orderForm").addEventListener("submit", function(e){
     e.preventDefault();
 
-    document.getElementById("msg").innerHTML =
-        "ধন্যবাদ! আপনার অর্ডার গ্রহণ করা হয়েছে।";
+    alert("ধন্যবাদ! আপনার অর্ডার গ্রহণ করা হয়েছে। খুব শীঘ্রই আমরা আপনার সাথে যোগাযোগ করবো।");
 
     this.reset();
+});
+
+// Smooth Scroll
+document.querySelector(".btn").addEventListener("click", function(e){
+    e.preventDefault();
+
+    document.querySelector("#order").scrollIntoView({
+        behavior:"smooth"
+    });
 });
